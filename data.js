7b25ec5a0572e33808fe7abae5423a39551024e5
jaybarve <jay52907420@gmail.com>
2025-12-14 15:57:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+<script>
+const kandaDatabase = [
+  {
+    traderId: 1,
+    traderName: "Jay Barve",
+    quantity: 20,
+    farmers: [
+      { name: "राम पाटील", village: "बार्शी", mobile: "9XXXXXXXXX" },
+      { name: "सुरेश देशमुख", village: "मोहोळ", mobile: "8XXXXXXXXX" }
+    ]
+  },
+  {
+    traderId: 2,
+    traderName: "ABC Traders",
+    quantity: 35,
+    farmers: [
+      { name: "महेश जाधव", village: "सोलापूर", mobile: "7XXXXXXXXX" }
+    ]
+  }
+];
+</script>
