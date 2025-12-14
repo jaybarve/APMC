c9cbd48e8f189573d067b33994726a26e27d7ec7
jaybarve <jay52907420@gmail.com>
2025-12-14 16:04:10 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,11 @@
-<script>
 const kandaDatabase = [
   {
     traderId: 1,
     traderName: "Jay Barve",
     quantity: 20,
     farmers: [
-      { name: "राम पाटील", village: "बार्शी", mobile: "9XXXXXXXXX" },
-      { name: "सुरेश देशमुख", village: "मोहोळ", mobile: "8XXXXXXXXX" }
+      { name: "राम पाटील", village: "बार्शी", mobile: "9876543210" },
+      { name: "सुरेश देशमुख", village: "मोहोळ", mobile: "9123456780" }
     ]
   },
   {
@@ -14,8 +13,7 @@ const kandaDatabase = [
     traderName: "ABC Traders",
     quantity: 35,
     farmers: [
-      { name: "महेश जाधव", village: "सोलापूर", mobile: "7XXXXXXXXX" }
+      { name: "महेश जाधव", village: "सोलापूर", mobile: "9988776655" }
     ]
   }
 ];
-</script>
